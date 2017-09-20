0e68cdfde122c2fff2b14889ee77d7d7e646c1e5
Jimmy's Mac <JimmyMacBook@Mackintoshs-MacBook-Pro.local>
2017-09-21 00:06:25 +0800

    first commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles=[
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'this is the 1st article I have created to show the effects',
+            'author':'Jimmy Zhang',
+            'create_date':'13-09-2017'
+        },
+        {
+            'id':2,
+            'title':'Article Two',
+            'body':'this is the 1st article I have created to show the effects',
+            'author':'Jimmy Zhang',
+            'create_date':'13-09-2017'
+        },
+        {
+            'id':3,
+            'title':'Article Three',
+            'body':'this is the 1st article I have created to show the effects',
+            'author':'Jimmy Zhang',
+            'create_date':'13-09-2017'
+        }
+    ]
+    return articles
